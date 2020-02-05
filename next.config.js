238d688444122eb412efc97272f58dc1a6fd9570
ErnieBuyco <buycoernie@gmail.com>
2020-02-05 16:44:59 +0800

    added-next-config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
+const withFonts = require('next-fonts');
+
+module.exports = withFonts(withCSS(withSass({
+      
+})))
