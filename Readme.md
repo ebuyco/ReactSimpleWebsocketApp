30a0504a70c2b15e5af8208374e80e10d14af9d1
ErnieBuyco <buycoernie@gmail.com>
2020-02-05 18:15:52 +0800

    well

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# ReactSimpleWebsocketApp
+# ReactSimpleWebsocketApp
--- server.js
+++ server.js
@@ -22,5 +22,27 @@ const ssrCache = cacheableResponse({
 })
 
 app.render().then(() => {
-        const server = express()
+        const server = express();
+        server.use(( res, next) => {
+          res.header('Access-Control-Allow-Origin', '*');
+          res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+          next();
+        });
+
+        server.use(cookieParser());
+        server.use(bodyParser.json());
+        server.use(bodyParser.urlencoded({ extended: true }));
+        // server.get('/', (req,res) => ssrCache({ req, res, pagePath: '/'}))
+
+        server.get('/', (req, res) => {
+            const queryParams = { id: req.params.id }
+            const pagePath = '/'
+            return ssrCache({ req, res, pagePath, queryParams })
+          })
+          server.get('*', (req, res) => handle(req, res))
+
+        server.listen(PORT, (err) => {
+          if (err) throw err
+          console.log(`> Ready on http://localhost:${PORT}`)
+        })
 })
