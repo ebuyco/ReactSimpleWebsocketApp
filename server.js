fcf7291eefce354bac7aeada46bdb65135c23715
ErnieBuyco <buycoernie@gmail.com>
2020-02-07 02:28:45 +0800

    socket-server

--- server.js
+++ server.js
@@ -25,6 +25,23 @@ nextApp.prepare().then(() => {
               console.log('message: ' + JSON.stringify(msg));
              io.emit('chat message', msg);
           })
+
+          socket.on('sendMessage', (message, callback) => {
+            const user = getUser(socket.id);
+
+            io.to(user.room).emit('message', { user: user.name, text: message });
+
+            callback();
+          });
+
+          socket.on('disconnect', () => {
+            const user = removeUser(socket.id);
+
+            if(user) {
+              io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+              io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+            }
+          })
     }
       );
 
